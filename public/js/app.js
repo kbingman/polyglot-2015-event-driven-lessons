f1a53ef44212b17c4cf504ce551561a6cf696ba3
Keith Bingman <kb@wantering.com>
2015-05-18 14:40:44 -0700

    notes for instagram api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 // Application code goes here
+
+// Media API
+// https://api.instagram.com/v1/media/{media-id}?client_id=API_KEY
+
+// Tag API
+// https://api.instagram.com/v1/tags/{tagname}/media/recent?client_id=API_KEY
