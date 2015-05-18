6f964b9fbc8e5cf0b7e7d7896f9e6cbbc9ee48f2
Keith Bingman <kb@wantering.com>
2015-05-18 14:35:11 -0700

    jquery lesson for search query

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,52 @@
-// Application code goes here
+var API_KEY = 'XXXX';
+
+// UI - Interaction handlers
+$('[data-search]').on('submit', listenForQuery);
+
+// API - Application Events
+$(document).on('instagram:results', renderResults);
+$(document).on('instagram:search', searchInstagram);
+
+function listenForQuery(e) {
+    e.preventDefault();
+    var query = $(e.target).find('[name=query]').val();
+    query = query.replace(/\s/g, '');
+
+    $(document).trigger('instagram:search', { query: query });
+}
+
+function renderResults(e, response) {
+    var html = response.data.map(function(record) {
+        var $div = $('<div></div>');
+        var $img = $('<img src=' + record.images.thumbnail.url + '>');
+
+        return $div.addClass('thumbnail').html($img);
+    })
+    $('[data-results]').html(html);
+    $('[data-more]')
+        .attr('data-query', response.query)
+        .attr('data-maxid', response.pagination.next_max_tag_id);
+}
+
+function getAjaxOptions(query) {
+    var url = 'https://api.instagram.com/v1/tags/' + data.query + '/media/recent';
+    return {
+        url: getTaggedPhotosURL(data.query),
+        jsonp: 'callback',
+        dataType: 'jsonp',
+        data: {
+            client_id: API_KEY
+        },
+    }
+};
+
+function searchInstagram(e, data) {
+    return $.ajax(getAjaxOptions(query))
+        .done(function(response) {
+            response.query = data.query;
+            $(document).trigger('instagram:results', response);
+        })
+        .error(function(response) {
+            console.log(response);
+        });
+}
